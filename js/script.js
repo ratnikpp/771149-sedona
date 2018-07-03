b9a721c9fc3a3e9239b18d7e8fd869b90dd7dd4e
Ratnikov Pavel <39724877+ratnikpp@users.noreply.github.com>
2018-07-03 21:33:47 +0300

    03.07 21.33

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var popup = document.querySelector(".search-modal");
+var open = document.querySelector(".btn-search");
+
+
+open.addEventListener("click", function(event) {
+  event.preventDefault(event);
+  popup.classList.toggle("modal-show");
+});
+
+
