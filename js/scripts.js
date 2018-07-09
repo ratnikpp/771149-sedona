b5f477a0a16a1a5f2e83be4281cb4ebfb018fdc2
Ratnikov Pavel <39724877+ratnikpp@users.noreply.github.com>
2018-07-09 17:14:14 +0300

    09.07.18

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,20 @@ var form = popup.querySelector("form");
 var login = popup.querySelector("[name=arrival]");
 var password = popup.querySelector("[name=arrival]");
 
+window.onload = function(event) {
+    popup.classList.toggle("modal-show");
+}
+
 open.addEventListener("click", function(event) {
-  event.preventDefault(event);
-  popup.classList.toggle("modal-show");
-   popup.classList.toggle("modal-error");
+    event.preventDefault(event);
+    popup.classList.toggle("modal-show");
+    popup.classList.toggle("modal-error");
 });
 
 form.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-  popup.classList.toggle("modal-error");
+    evt.preventDefault();
+    popup.classList.toggle("modal-error");
+    $(".search-modal").hide();
 });
 
 
